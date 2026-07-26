34ef3b4dc374ef1259159968b698d0c328b34c4c
Ayushi Agarwal <agarwalayushi1978@gmail.com>
2026-07-26 12:41:29 +0530

    Delete netlify/functions directory

--- netlify/functions/api.py
+++
@@ -1,6 +0,0 @@
-from app import app  # Ensure 'app' matches your main Flask file name (e.g., app.py)
-import serverless_wsgi
-
-
-def handler(event, context):
-  return serverless_wsgi.handle_request(app, event, context)
